30bda5e30e3749fdede14faf9258a354fcf5106f
Matthias Radtke <matthias.radtke@codecentric.de>
2020-10-14 20:05:41 +0200

    Remove tensorflow and keras dvc introduction and use wine quality showcase

--- notebooks/dvc/dvc_introduction.py
+++ notebooks/dvc/dvc_introduction.py
@@ -1,87 +1,80 @@
+import logging
+
 import fire
-import keras
-import pickle
-import tensorflow as tf
-from tensorflow.python.saved_model import signature_constants
-from tensorflow.python.saved_model import tag_constants
-from tensorflow.python.saved_model import builder
+import yaml
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import ElasticNet
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+import mlflow.sklearn
+
+PATH = 'data/'
 
 
-def configure(outpath):
-    generator = keras.preprocessing.image.ImageDataGenerator(rescale=1. / 255)
-    with open(outpath, "wb") as f:
-        pickle.dump(generator, f)
+def download_data(url: str, path: str):
+    data = pd.read_csv(url, sep=';')
+
+    logger = logging.getLogger(__name__)
+    logger.info(f"Data downloaded from {url}")
+
+    data.to_csv(path, index=False)
 
 
-def build_generator(config_path,
-                    dataset_path,
-                    data_set_type,
-                    img_height=100, img_width=100):
-    with open(config_path, "rb") as f:
-        generator = pickle.load(f)
-    path = "%s/%s" % (dataset_path, data_set_type)
-    return generator.flow_from_directory(path,
-                                         target_size=(img_height,
-                                                      img_width),
-                                         color_mode='rgb')
+def split_data(data_path: str):
+    data = pd.read_csv(data_path)
+
+    x = data.drop(["quality"], axis=1)
+
+    y = data["quality"]
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+    x_train.to_csv(PATH + 'x_train.csv', index=False)
+    y_train.to_csv(PATH + 'y_train.csv', index=False)
+    x_test.to_csv(PATH + 'x_test.csv', index=False)
+    y_test.to_csv(PATH + 'y_test.csv', index=False)
 
 
-def define_model(input_shape, num_classes):
-    model = keras.models.Sequential()
-    model.add(
-        keras.layers.Conv2D(filters=4, kernel_size=(2, 2), strides=1, activation='relu', input_shape=input_shape))
-    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
-    model.add(keras.layers.Conv2D(filters=4, kernel_size=(2, 2), strides=1, activation='relu'))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
-    model.add(keras.layers.Dropout(rate=0.25))
-    model.add(keras.layers.Flatten())
-    model.add(keras.layers.Dense(units=8, activation='relu'))
-    model.add(keras.layers.Dropout(rate=0.5))
-    model.add(keras.layers.Dense(units=num_classes, activation='softmax'))
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=keras.optimizers.Adadelta(),
-                  metrics=['accuracy'])
-    return model
+def train_model(x_train_path: str, y_train_path: str):
+    x_train = pd.read_csv(x_train_path)
+    y_train = pd.read_csv(y_train_path)
+
+    with open('params.yaml', 'r') as fd:
+        params = yaml.safe_load(fd)
+
+    pipe = Pipeline([('scaler', StandardScaler()),
+                     ('regressor', ElasticNet(alpha=params['alpha'],
+                                              l1_ratio=params['l1_ratio'],
+                                              random_state=42))])
+
+    pipe.fit(x_train, y_train)
+
+    mlflow.sklearn.save_model(pipe, PATH + "model")
 
 
-def train_model(dataset_path, config_path, out_path, training_set="Training", epochs=2):
-    dataset = dataset_path
-    config = config_path
-    training_data = build_generator(config, dataset, training_set)
-    input_shape = training_data.image_shape
-    num_classes = len(training_data.class_indices)
-    model = define_model(input_shape, num_classes)
-    steps_per_epoch = training_data.samples // training_data.batch_size
-    model.fit_generator(training_data,
-                        steps_per_epoch=steps_per_epoch,
-                        epochs=epochs,
-                        verbose=0)
-    model.save(out_path)
+def evaluate_model(model_path: str, x_test_path: str, y_test_path: str):
+    pipe = mlflow.sklearn.load_model(model_path)
 
+    x_test = pd.read_csv(x_test_path)
+    y_test = pd.read_csv(y_test_path)
 
-def export(model_path, out_path):
-    model_path = model_path
-    model = keras.models.load_model(model_path)
-    tensor_info_input = tf.saved_model.utils.build_tensor_info(model.input)
-    tensor_info_output = tf.saved_model.utils.build_tensor_info(model.output)
-    prediction_signature = (
-        tf.saved_model.signature_def_utils.build_signature_def(
-            inputs={'input': tensor_info_input},
-            outputs={'prediction': tensor_info_output},
-            method_name=signature_constants.PREDICT_METHOD_NAME))
+    y_pred = pipe.predict(x_test)
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    mae = mean_absolute_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
 
-    export_path = out_path
-    tf_builder = builder.SavedModelBuilder(export_path)
-    with keras.backend.get_session() as sess:
-        tf_builder.add_meta_graph_and_variables(
-            sess=sess,
-            tags=[tag_constants.SERVING],
-            signature_def_map={
-                signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY: prediction_signature
-            }
-        )
-    tf_builder.save()
+    logger = logging.getLogger(__name__)
+    logger.info("Model has a coefficient R^2 of %.3f.", r2)
+
+    result = pd.DataFrame({"train": {"rmse": float(rmse),
+                                     "mae": float(mae),
+                                     "r2": float(r2)}})
+
+    result.to_json(PATH + 'result.json')
 
 
 if __name__ == '__main__':
