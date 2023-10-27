2cfab0c7f1c7e53f5702490a335b33b346eede62
Tim Sabsch <tim.sabsch@codecentric.de>
2023-10-27 16:30:53 +0200

    Code Quality

--- notebooks/cml/get_data.py
+++ notebooks/cml/get_data.py
@@ -1,18 +1,20 @@
-from sklearn.datasets import make_classification
-from sklearn.model_selection import train_test_split
-import numpy as np
 import os
 
+import numpy as np
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+
+
 seed = 42
-# Generate data
+# generate data
 X, y = make_classification(n_samples = 1000, random_state=seed)
 
-# Make a train test split
-X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=seed)
+# split into train / test data
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=seed)
 
 # Save it
 os.makedirs("data", exist_ok=True)
-np.savetxt("data/train_features.csv",X_train)
-np.savetxt("data/test_features.csv",X_test)
-np.savetxt("data/train_labels.csv",y_train)
-np.savetxt("data/test_labels.csv",y_test)
+np.savetxt("data/train_features.csv", X_train)
+np.savetxt("data/test_features.csv", X_test)
+np.savetxt("data/train_labels.csv", y_train)
+np.savetxt("data/test_labels.csv", y_test)
--- notebooks/mlflow/training_code.py
+++ notebooks/mlflow/training_code.py
@@ -1,16 +1,11 @@
 import logging
 
-import fire
 import yaml
-import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import ElasticNet
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-
-import mlflow.sklearn
 
 PATH = 'data/'
 
