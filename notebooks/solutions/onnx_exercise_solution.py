c592c181ac0bf05b6c3cff742fda3572c93a1814
Tim Sabsch <tim.sabsch@codecentric.de>
2024-12-04 17:37:46 +0100

    ONNX exercise (#3)

--- notebooks/solutions/onnx_exercise_solution.py
+++ notebooks/solutions/onnx_exercise_solution.py
@@ -3,11 +3,15 @@ import onnxruntime as rt
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+# create the fastapi application
 api = FastAPI()
 
+# load the onnx model. for our demo purposes, the model may live in the global context.
+# for more advanced apis, it may be reasonable to move the model into a dependency
 sess = rt.InferenceSession("model/loan_model.onnx")
 
 
+# define the features that our request expects
 class LoanFeatures(BaseModel):
     applicantincome: float
     coapplicantincome: float
@@ -22,29 +26,45 @@ class LoanFeatures(BaseModel):
 
     @classmethod
     def get_type_fields(cls, _type) -> set:
+        """Utility function to get all features of this BaseModel of a certain type.
+
+        Example usage:
+        `all_int_fields = LoanFeatures.get_type_fields(int)`  # result: {}
+        """
         return {
             field for field, fieldinfo in cls.model_fields.items()
             if fieldinfo.annotation == _type
         }
 
 
+# define the features that our response should send
 class Prediction(BaseModel):
-    label: int
+    label: str
     probability: float
 
 
+# create a POST endpoint that retrieves prediction requests, performs the prediction and
+# returns the prediction result as its response
 @api.post("/predict")
 def post_predict(loan_features: LoanFeatures) -> list[Prediction]:
+    # convert the request object into a dict of numpy arrays
+    # HINT: you can retrieve a dict with all features via loan_features.model_dump()
     ins = {k: np.array(v) for k, v in loan_features.model_dump().items()}
 
+    # convert the feature dtypes into the same dtypes that the model expects
+    # NOTE that the input_value is expected to be two-dimensional, so we need to reshape it properly
     for c in LoanFeatures.get_type_fields(float):
         ins[c] = ins[c].astype(np.float32).reshape((1, 1))
     for k in LoanFeatures.get_type_fields(str):
-        ins[k] = ins[k].astype(object).reshape((1, 1)).astype(object)
+        ins[k] = ins[k].reshape((1, 1))
 
+    # run the prediction
     labels, probabilities = sess.run(None, ins)
+
+    # create the response object
     predicted = []
     for k, v in probabilities[0].items():
         predicted.append(Prediction(label=k, probability=v))
 
+    # return the response
     return predicted
